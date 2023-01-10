041ff5bbfddd673675e3b7e56083f76e7617604b
rajammaldeepak <rajammal.deepak@gmail.com>
2023-01-10 13:50:26 +0530

    second commit of this project

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,5 +1,4 @@
 # Configuration file for the Sphinx documentation builder.
-#
 # For the full list of built-in configuration values, see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 
@@ -24,7 +23,8 @@ exclude_patterns = []
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
+import sphinx_rtd_theme
 
-html_theme = 'yummy_sphinx_theme'
+html_theme = 'sphinx_rtd_theme'
 
 html_static_path = ['_static']
